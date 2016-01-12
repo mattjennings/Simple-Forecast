840a7450042fb979fc79c5a0bbc4da65215bcf3c
mattjennings <mattjennings44@gmail.com>
2016-01-11 21:05:09 -0600

    Initial commit

--- WeatherApp/RootViewController.swift
+++ WeatherApp/RootViewController.swift
@@ -1,6 +1,6 @@
 //
 //  RootViewController.swift
-//  WeatherApp
+//  PageBasedApplication
 //
 //  Created by Matt Jennings on 2016-01-11.
 //  Copyright © 2016 Matt Jennings. All rights reserved.
@@ -9,44 +9,53 @@
 import UIKit
 
 class RootViewController: UIViewController, UIPageViewControllerDelegate {
-
+    
     var pageViewController: UIPageViewController?
-
-
+    
+    @IBOutlet weak var viewFrame: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         // Configure the page view controller and add it as a child view controller.
-        self.pageViewController = UIPageViewController(transitionStyle: .PageCurl, navigationOrientation: .Horizontal, options: nil)
+        self.pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
         self.pageViewController!.delegate = self
-
+        
         let startingViewController: DataViewController = self.modelController.viewControllerAtIndex(0, storyboard: self.storyboard!)!
         let viewControllers = [startingViewController]
         self.pageViewController!.setViewControllers(viewControllers, direction: .Forward, animated: false, completion: {done in })
-
+        
         self.pageViewController!.dataSource = self.modelController
-
+        
         self.addChildViewController(self.pageViewController!)
         self.view.addSubview(self.pageViewController!.view)
-
+        
         // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-        var pageViewRect = self.view.bounds
+        /*
+        var pageViewRect = self.view.frame
         if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            pageViewRect = CGRectInset(pageViewRect, 40.0, 40.0)
+        pageViewRect = CGRectInset(pageViewRect, 40.0, 40.0)
         }
         self.pageViewController!.view.frame = pageViewRect
-
+        */
+        
         self.pageViewController!.didMoveToParentViewController(self)
-
+        
         // Add the page view controller's gesture recognizers to the book view controller's view so that the gestures are started more easily.
         self.view.gestureRecognizers = self.pageViewController!.gestureRecognizers
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        // After constraints are applied
+        self.pageViewController!.view.frame = viewFrame.frame
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     var modelController: ModelController {
         // Return the model controller object, creating it if necessary.
         // In more complex implementations, the model controller may be passed to the view controller.
@@ -55,26 +64,26 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
         }
         return _modelController!
     }
-
+    
     var _modelController: ModelController? = nil
-
+    
     // MARK: - UIPageViewController delegate methods
-
+    
     func pageViewController(pageViewController: UIPageViewController, spineLocationForInterfaceOrientation orientation: UIInterfaceOrientation) -> UIPageViewControllerSpineLocation {
         if (orientation == .Portrait) || (orientation == .PortraitUpsideDown) || (UIDevice.currentDevice().userInterfaceIdiom == .Phone) {
             // In portrait orientation or on iPhone: Set the spine position to "min" and the page view controller's view controllers array to contain just one view controller. Setting the spine position to 'UIPageViewControllerSpineLocationMid' in landscape orientation sets the doubleSided property to true, so set it to false here.
             let currentViewController = self.pageViewController!.viewControllers![0]
             let viewControllers = [currentViewController]
             self.pageViewController!.setViewControllers(viewControllers, direction: .Forward, animated: true, completion: {done in })
-
+            
             self.pageViewController!.doubleSided = false
             return .Min
         }
-
+        
         // In landscape orientation: Set set the spine location to "mid" and the page view controller's view controllers array to contain two view controllers. If the current page is even, set it to contain the current and next view controllers; if it is odd, set the array to contain the previous and current view controllers.
         let currentViewController = self.pageViewController!.viewControllers![0] as! DataViewController
         var viewControllers: [UIViewController]
-
+        
         let indexOfCurrentViewController = self.modelController.indexOfViewController(currentViewController)
         if (indexOfCurrentViewController == 0) || (indexOfCurrentViewController % 2 == 0) {
             let nextViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerAfterViewController: currentViewController)
@@ -84,10 +93,10 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
             viewControllers = [previousViewController!, currentViewController]
         }
         self.pageViewController!.setViewControllers(viewControllers, direction: .Forward, animated: true, completion: {done in })
-
+        
         return .Mid
     }
-
-
+    
+    
 }
 
